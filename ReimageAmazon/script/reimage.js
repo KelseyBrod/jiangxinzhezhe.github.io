8a4473930deb06603099266c39f9a7890a86e8be
jiangxinzhezhe <xj46@duke.edu>
2022-11-24 17:57:10 -0500

    reimage amazon index change

---
+++ ReimageAmazon/script/reimage.js
@@ -0,0 +1,6 @@
+function ship() {
+        console.log('Button clicked')
+        const el = document.getElementsByClassName("shipping");
+        console.log(el);
+        document.getElementsByClassName("shipping").style.display = "block";
+}
